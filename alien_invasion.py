06b5f46ff530cefb156fb05c1abd9c85187599b8
royal <royalhuseyn902@gmail.com>
2020-08-24 13:50:49 +0400

    Added a scoreboard.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats 
+from scoreboard import Scoreboard 
 from button import Button 
 from ship import Ship
 from bullet import Bullet
@@ -23,8 +24,10 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics,
+        #   and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -65,12 +68,13 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos) 
         if button_clicked and not self.stats.game_active:
             # Reset the game settings.
-            self.settings.initialize_dynamic_settings()
             self._start_game()
-
+            
     def _start_game(self):
         '''Start the game.'''
         # Reset the game statistics.
+        self.settings.initialize_dynamic_settings()
+        
         self.stats.reset_stats()
         self.stats.game_active = True
 
@@ -230,6 +234,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
 
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
 		self.ship_limit = 3
 
 		# Bullet settings
-		self.bullet_width = 3   
+		self.bullet_width = 3.0 
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullets_allowed = 3
@@ -28,8 +28,8 @@ class Settings:
 	def initialize_dynamic_settings(self):
 		"""Initialize settings that can change throughout the game."""
 		self.ship_speed = 1.5
-		self.bullet_speed = 3.0
-		self.alien_speed = 1.0
+		self.bullet_speed = 3.0    
+		self.alien_speed = 1.0  
 
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1
